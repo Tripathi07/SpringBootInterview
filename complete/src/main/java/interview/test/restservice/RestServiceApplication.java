1d9c443ed4365eb754bef281cac0ac9540f8167b
Tripathi07 <consulttripathi@gmail.com>
2023-02-28 17:20:30 +0100

    Updaitng Application class file

--- complete/src/main/java/interview/test/restservice/RestServiceApplication.java
+++ complete/src/main/java/interview/test/restservice/RestServiceApplication.java
@@ -1,1 +1,14 @@
 // Here we need to define the Main class
+package interview.test.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
